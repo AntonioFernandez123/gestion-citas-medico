6e696c3b438f29722509c3771934dc1bd3eb1d81
AntonioFernandezLopez <2860075@alu.murciaeduca.es>
2024-03-20 18:12:18 +0100

    botones

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+export default function Header() {
+  return (
+    <div>
+      <button>Pacientes</button>
+      <button>Medicos</button>
+      <button>Citas</button>
+      <button>Diagnosticos</button>
+    </div>
+  );
+} 
